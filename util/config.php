83a109b6753af6ead9fdef7adf08a07af75dc31c
Daniel Paulo de Assis <daniel@dpassis.com.br>
2016-05-23 14:27:15 -0300

    Update config.php

--- util/config.php
+++ util/config.php
@@ -3,6 +3,6 @@
 define('DB_TYPE', 'mysql');
 define('DB_HOST','localhost');
 define('DB_NAME','dbprodutos');
-define('DB_USER','dpassis');
-define('DB_PASS','thedoors');
+define('DB_USER','root');
+define('DB_PASS','');
 define('DB_ENCODING', 'utf8');
